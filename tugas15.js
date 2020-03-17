b634e38c0dccead5160ba80ebc6209ce76b5ce45
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:23:00 +0700

    tugas niomic

---
+++ tugas15.js
@@ -0,0 +1,10 @@
+function tugas15() {
+    let AB = 8;
+    let BC = 6;
+    let AC = Math.sqrt(Math.pow(AB,2)+Math.pow(BC,2));
+    console.log("Nilai AB : " + AB);
+    console.log("Nilai BC : " + BC);
+    console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah : " + AC + " cm");
+};
+
+tugas15();
